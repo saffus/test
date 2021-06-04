5e80b85e93e1f80709ca81519093f33f63cefb50
saffan <saffanms@yahoo.com>
2021-06-04 23:25:19 +0530

    change value of int in login controller

--- LoginService/src/main/java/myApp/LoginService/Controller/LoginController.java
+++ LoginService/src/main/java/myApp/LoginService/Controller/LoginController.java
@@ -20,7 +20,7 @@ public class LoginController {
 	@Autowired
 	LoginResponse resp;
 	
-	private int i =11;
+	private int i =14;
 
 	@RequestMapping(name = "/Login", method = RequestMethod.GET)
 	public ResponseEntity<LoginResponse> LoginValidation(@RequestParam("uname") String name,
