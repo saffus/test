6acffbde94220c0368b1d4adee28f79a8a325d70
saffan <saffanms@yahoo.com>
2021-07-06 12:58:57 +0530

    tESTING WITH MOCKITO SUCCESS. Removed spring test dependency.

--- HibernateSpringMVC/src/test/java/service/test/SecurityServiceTest.java
+++ HibernateSpringMVC/src/test/java/service/test/SecurityServiceTest.java
@@ -1,5 +1,7 @@
 package service.test;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,15 +11,17 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.app.config.entity.User;
 import com.app.config.enums.EncryptionAlgorithm;
 import com.app.config.repo.UserRepo;
 import com.app.config.root.security.CustomUserDetailsService;
 
-@ExtendWith(SpringExtension.class)
+
+
 @ExtendWith(MockitoExtension.class)
 public class SecurityServiceTest {
 	
@@ -28,10 +32,24 @@ public class SecurityServiceTest {
 	UserRepo ur;
 	
 	@Test
-	public void loadUserTest() {
+	public void loadUserTestFailure() {
 		User u = new User("saffan","admin", EncryptionAlgorithm.BCRYPT );
 		
 		Mockito.when(ur.getUser("saffan")).thenThrow(UsernameNotFoundException.class);
+		
+		
+		Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+			
+			cu.loadUserByUsername("saffan").getUsername();});
+			
+		
+	}
+	
+	@Test
+	public void loadUserTest() {
+		User u = new User("saffan","admin", EncryptionAlgorithm.BCRYPT );
+		
+		Mockito.when(ur.getUser("saffan")).thenReturn(Optional.of(u));
 		
 		String us =	cu.loadUserByUsername("saffan").getUsername();
 		
